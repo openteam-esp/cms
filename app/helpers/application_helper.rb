e7801fbde429f2064ee5942a56b77c8d83b56164
Evgeny Lapin <lev@openteam.ru>
2011-11-02 14:57:59 +0700

    #8646. composed title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+
+  def composed_title
+    titles = []
+    model  = "pluralize.#{params[:controller].singularize}"
+    case params[:action]
+    when 'index'
+      titles << "#{I18n.t('actions.index')} #{I18n.t(model, :count => 5)}"
+    when 'show'
+      titles << resource
+    when 'new', 'create'
+      titles << "#{I18n.t('actions.new')} #{I18n.t(model, :count => 2)}"
+    when 'edit', 'update'
+      titles << "#{I18n.t('actions.edit')} #{I18n.t(model, :count => 2)}"
+    end
+    titles << I18n.t('application')
+    titles.join(" | ")
+  end
+
 end
