039e47737632525794a85d8dbfc8999efc68444d
Dmitry Lihachev <lda@openteam.ru>
2011-11-02 11:43:45 +0700

    Работает консоль

--- config/application.rb
+++ config/application.rb
@@ -33,17 +33,31 @@ module Cms
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Asia/Novosibirsk'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :ru
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    # Enable the asset pipeline
+    config.assets.enabled = true
+
+    # Version of your assets, change this if you want to expire all your assets
+    config.assets.version = '1.0'
+
+    config.generators do |generators|
+      generators.test_framework       :rspec, :fixture => true
+      generators.fixture_replacement  :fabrication
+      generators.stylesheet_engine    :sass
+    end
+
+    config.sass.preferred_syntax = :sass if config.respond_to? :sass
 
   end
 end
