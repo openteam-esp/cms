2516cf262d9a510e56d6fcd2e4aaca3e49bf3f92
Manin Iliy <bkmzvf@gmail.com>
2011-11-03 10:23:01 +0700

    Перепилил app_helper, для правильного отображения заголовка страницы

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,22 @@
 module ApplicationHelper
 
-  def action_title(controller = params[:controller], action = params[:action])
-    model  = "pluralize.#{controller.singularize}"
-    action = case action
-    when 'show'
-      return resource
-    when 'create'
-      'new'
-    when 'update'
-      'edit'
-    else
-      action
+  def action_title(options={})
+    controller = options[:controller] || params[:controller]
+    action = options[:action] || params[:action]
+    model  = "pluralize.#{controller.to_s.singularize}"
+    action_conf = case action
+      when 'show'
+        return resource
+      when 'create'
+        { :action => 'new', :count => 1 }
+      when 'update'
+        { :action => 'edit', :count => 1 }
+      when 'index'
+        { :action => action, :count => 10 }
+      else
+        { :action => action, :count => 2 }
     end
-    "#{I18n.t("actions.#{action}")} #{I18n.t(model, :count => 2)}"
+    "#{I18n.t("actions.#{action_conf[:action]}")} #{I18n.t(model, :count => action_conf[:count])}"
   end
 
   def title
