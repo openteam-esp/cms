eca9188b93e547ec246a5540e784f10e8c37e8ac
Manin Iliy <bkmzvf@gmail.com>
2011-11-17 14:49:36 +0700

    запилили NewsItemPart канал

--- app/models/parts/news_item_part.rb
+++ app/models/parts/news_item_part.rb
@@ -1,9 +1,10 @@
 # encoding: utf-8
 
 class NewsItemPart < Part
+  validates_presence_of :news_channel
 
   def request
-    @request ||= Restfulie.at("#{news_url}/public/entries/#{params['slug']}").accepts("application/json").get
+    @request ||= Restfulie.at("#{news_url}/public/channels/#{news_channel}/entries/#{params['slug']}").accepts("application/json").get
   end
 
   def to_json
@@ -19,3 +20,25 @@ class NewsItemPart < Part
       "#{Settings['news.protocol']}://#{Settings['news.host']}:#{Settings['news.port']}"
     end
 end
+
+# == Schema Information
+#
+# Table name: parts
+#
+#  id                       :integer         not null, primary key
+#  html_content_id          :integer
+#  created_at               :datetime
+#  updated_at               :datetime
+#  region                   :string(255)
+#  type                     :string(255)
+#  node_id                  :integer
+#  navigation_end_level     :integer
+#  navigation_from_id       :integer
+#  navigation_default_level :integer
+#  news_channel             :string(255)
+#  news_order_by            :string(255)
+#  news_until               :date
+#  news_per_page            :integer
+#  news_paginated           :boolean
+#
+
