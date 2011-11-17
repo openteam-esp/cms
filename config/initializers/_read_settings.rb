6af44eb4b8a90241d9d651ebdfa481136a4d5515
Manin Iliy <bkmzvf@gmail.com>
2011-11-17 15:54:57 +0700

    fix read settings

--- config/initializers/_read_settings.rb
+++ config/initializers/_read_settings.rb
@@ -10,4 +10,8 @@ Settings.define 's3.access_key_id',           :env_var => 'S3_ACCESS_KEY_ID'
 Settings.define 's3.secret_access_key',       :env_var => 'S3_SECRET_ACCESS_KEY'
 Settings.define 's3.bucket_name',             :env_var => 'S3_BUCKET_NAME'
 
+Settings.define 'news.host',             :env_var => 'NEWS_HOST'
+Settings.define 'news.protocol',         :env_var => 'NEWS_PROTOCOL'
+Settings.define 'news.port',             :env_var => 'NEWS_PORT'
+
 Settings.resolve!
