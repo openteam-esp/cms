eef5b78b717d775c97b581d66c7c903a4562a03f
Evgeniy Serykh <e.v.serykh@gmail.com>
2011-11-22 10:34:36 +0700

    blue_pages settings

--- config/initializers/_read_settings.rb
+++ config/initializers/_read_settings.rb
@@ -10,8 +10,12 @@ Settings.define 's3.access_key_id',           :env_var => 'S3_ACCESS_KEY_ID'
 Settings.define 's3.secret_access_key',       :env_var => 'S3_SECRET_ACCESS_KEY'
 Settings.define 's3.bucket_name',             :env_var => 'S3_BUCKET_NAME'
 
-Settings.define 'news.host',             :env_var => 'NEWS_HOST'
-Settings.define 'news.protocol',         :env_var => 'NEWS_PROTOCOL'
-Settings.define 'news.port',             :env_var => 'NEWS_PORT'
+Settings.define 'blue_pages.host',            :env_var => 'BLUE_PAGES_HOST'
+Settings.define 'blue_pages.protocol',        :env_var => 'BLUE_PAGES_PROTOCOL'
+Settings.define 'blue_pages.port',            :env_var => 'BLUE_PAGES_PORT'
+
+Settings.define 'news.host',                  :env_var => 'NEWS_HOST'
+Settings.define 'news.protocol',              :env_var => 'NEWS_PROTOCOL'
+Settings.define 'news.port',                  :env_var => 'NEWS_PORT'
 
 Settings.resolve!
