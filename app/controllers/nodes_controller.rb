b0d71a7f597271c0440c94ea42979d89c0176bda
Evgeny Lapin <lev@openteam.ru>
2011-12-22 10:33:10 +0700

    find_by_route with /

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -1,6 +1,6 @@
 class NodesController < ApplicationController
   def show
-    @node = Node.find_by_route(params[:id])
+    @node = Node.find_by_route(params[:id]+'/')
     @node.parts_params = params[:parts_params] || {}
 
     respond_to do |format|
