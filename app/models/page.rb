0e52357bd3db6e568b7487be5480874f6c9c25d7
Manin Iliy <bkmzvf@gmail.com>
2012-02-15 09:45:11 +0700

    annotate

--- app/models/page.rb
+++ app/models/page.rb
@@ -29,20 +29,22 @@ end
 #
 # Table name: nodes
 #
-#  id                  :integer         not null, primary key
-#  slug                :string(255)
-#  title               :string(255)
-#  ancestry            :string(255)
-#  type                :string(255)
-#  created_at          :datetime
-#  updated_at          :datetime
-#  route               :text
-#  template            :string(255)
-#  client_url          :string(255)
-#  in_navigation       :boolean
-#  navigation_group    :string(255)
-#  navigation_position :integer
-#  navigation_title    :string(255)
-#  ancestry_depth      :integer         default(0)
+#  id                   :integer         not null, primary key
+#  slug                 :string(255)
+#  title                :string(255)
+#  ancestry             :string(255)
+#  type                 :string(255)
+#  created_at           :datetime
+#  updated_at           :datetime
+#  route                :text
+#  template             :string(255)
+#  client_url           :string(255)
+#  in_navigation        :boolean
+#  navigation_group     :string(255)
+#  navigation_position  :integer
+#  navigation_title     :string(255)
+#  ancestry_depth       :integer         default(0)
+#  page_for_redirect_id :integer
+#  weight               :string(255)
 #
 
--- spec/models/part_spec.rb
+++ spec/models/part_spec.rb
@@ -10,28 +10,41 @@ end
 #
 # Table name: parts
 #
-#  id                       :integer         not null, primary key
-#  created_at               :datetime
-#  updated_at               :datetime
-#  region                   :string(255)
-#  type                     :string(255)
-#  node_id                  :integer
-#  navigation_end_level     :integer
-#  navigation_from_id       :integer
-#  navigation_default_level :integer
-#  news_channel             :string(255)
-#  news_order_by            :string(255)
-#  news_until               :date
-#  news_per_page            :integer
-#  news_paginated           :boolean
-#  news_item_page_id        :integer
-#  blue_pages_category_id   :integer
-#  appeal_section_slug      :string(255)
-#  blue_pages_expand        :boolean
-#  navigation_group         :string(255)
-#  title                    :string(255)
-#  html_info_path           :string(255)
-#  blue_pages_item_page_id  :integer
-#  documents_kind           :string(255)
+#  id                          :integer         not null, primary key
+#  created_at                  :datetime
+#  updated_at                  :datetime
+#  region                      :string(255)
+#  type                        :string(255)
+#  node_id                     :integer
+#  navigation_end_level        :integer
+#  navigation_from_id          :integer
+#  navigation_default_level    :integer
+#  news_channel                :string(255)
+#  news_order_by               :string(255)
+#  news_until                  :date
+#  news_per_page               :integer
+#  news_paginated              :boolean
+#  news_item_page_id           :integer
+#  blue_pages_category_id      :integer
+#  appeal_section_slug         :string(255)
+#  blue_pages_expand           :boolean
+#  navigation_group            :string(255)
+#  title                       :string(255)
+#  html_info_path              :string(255)
+#  blue_pages_item_page_id     :integer
+#  documents_kind              :string(255)
+#  documents_item_page_id      :integer
+#  documents_paginated         :boolean
+#  documents_per_page          :integer
+#  documents_context_id        :integer
+#  youtube_resource_id         :string(255)
+#  youtube_item_page_id        :integer
+#  youtube_video_resource_id   :string(255)
+#  youtube_resource_kind       :string(255)
+#  youtube_per_page            :integer
+#  youtube_paginated           :boolean
+#  youtube_video_resource_kind :string(255)
+#  news_height                 :integer
+#  news_width                  :integer
 #
 
