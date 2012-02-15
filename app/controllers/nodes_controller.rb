f239d0e4b3f5ddbcfc5777ca63f92d14be8e33fd
Evgeniy Serykh <e.v.serykh@gmail.com>
2012-02-15 12:41:25 +0700

    возвращать 404 в NodesController#show если нода не найдена

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -1,6 +1,9 @@
 class NodesController < ApplicationController
   def show
     @node = Node.find_by_route(params[:id])
+
+    render :file => "#{Rails.root}/public/404.html", :layout => false, :status => 404 and return unless @node
+
     @node.parts_params = params[:parts_params] || {}
 
     respond_to do |format|
