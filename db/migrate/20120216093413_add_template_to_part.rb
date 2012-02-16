7899f31bc8277286c4c6882f75862c01720157df
Evgeniy Serykh <e.v.serykh@gmail.com>
2012-02-16 17:19:59 +0700

    #9297. Проставить шаблон для парт в миграции

--- db/migrate/20120216093413_add_template_to_part.rb
+++ db/migrate/20120216093413_add_template_to_part.rb
@@ -1,6 +1,13 @@
 class AddTemplateToPart < ActiveRecord::Migration
-  def change
+  def up
     add_column :parts, :template, :string
 
+    Part.all.each do |part|
+      part.update_attribute(:template, part.type.underscore)
+    end
+  end
+
+  def down
+    remove_column :parts, :template
   end
 end
