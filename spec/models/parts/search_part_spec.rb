615673bebf6597de684c215b0e1960404b515581
Evgeniy Serykh <e.v.serykh@gmail.com>
2012-04-20 11:45:57 +0700

    test for SearchPart

--- spec/models/parts/search_part_spec.rb
+++ spec/models/parts/search_part_spec.rb
@@ -10,6 +10,15 @@ describe SearchPart do
   its(:search_per_page) { should == 15 }
 
   context 'url_for_request' do
-    let(:expected_url) { "#{Settings['search_engine.url']}?q=&page=1&per_page=15" }
+    let(:expected_url) { "#{Settings['search_engine.url']}?url=http://example.com&q=&page=1&per_page=15" }
+
+    let(:mock_object) { Object }
+
+    before { mock_object.stub(:response_status).and_return(200) }
+    before { mock_object.stub(:response_hash).and_return({}) }
+
+    before { Requester.should_receive(:new).with(expected_url, 'application/json').and_return(mock_object) }
+
+    it { subject.to_json }
   end
 end
