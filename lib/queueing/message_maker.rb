120f4cd1eb6bde8b21bcbd9e62bb067f0e251858
Evgeniy Serykh <e.v.serykh@gmail.com>
2012-04-25 10:25:45 +0700

    send messages via topic exchange

--- lib/queueing/message_maker.rb
+++ lib/queueing/message_maker.rb
@@ -1,12 +1,17 @@
 require 'bunny'
 
 class MessageMaker
-  def self.make_message(queue, message)
+  def self.make_message(queue_name, message)
     amqp_client = Bunny.new(:logging => false)
+
     amqp_client.start
-    queue = amqp_client.queue(queue, :durable => true)
-    direct_exchange = amqp_client.exchange('');
-    direct_exchange.publish(message, :key => queue.name, :persistent => true)
+
+    exchange = amqp_client.exchange('esp', :type => :topic)
+
+    queue = amqp_client.queue(queue_name, :durable => true)
+
+    exchange.publish(message, :key => queue.name)
+
     amqp_client.stop
   end
 end
