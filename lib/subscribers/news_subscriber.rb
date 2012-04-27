ae8f7238ac52111a8fd9b7202ef03ba23732916c
Evgeniy Serykh <e.v.serykh@gmail.com>
2012-04-27 08:35:45 +0700

    NewsSubscriber can remove news

--- lib/subscribers/news_subscriber.rb
+++ lib/subscribers/news_subscriber.rb
@@ -1,7 +1,16 @@
 class NewsSubscriber
   def publish(news)
-    NewsItemPart.where(:news_channel => news['channels']).each do |news_item_part|
-      MessageMaker.make_message 'esp.cms.searcher', 'add', news_item_part.url_with_slug(news['slug'])
-    end
+    send_message('add', news)
   end
+
+  def remove(news)
+    send_message('remove', news)
+  end
+
+  private
+    def send_message(message, news)
+      NewsItemPart.where(:news_channel => news['channels']).each do |news_item_part|
+        MessageMaker.make_message 'esp.cms.searcher', message, news_item_part.url_with_slug(news['slug'])
+      end
+    end
 end
--- spec/lib/subscribers/news_subscriber_spec.rb
+++ spec/lib/subscribers/news_subscriber_spec.rb
@@ -9,22 +9,31 @@ describe NewsSubscriber do
   let(:bar_page) { Fabricate :page, :parent => locale, :slug => 'bar' }
 
   let(:foo_part) { Fabricate :news_item_part, :node => foo_page, :news_channel => '1' }
-  let(:bar_part) { Fabricate :news_item_part, :node => bar_page, :news_channel => '1' }
+  let(:bar_part) { Fabricate :news_item_part, :node => bar_page, :news_channel => '3' }
 
   let(:baz_part) { Fabricate :news_item_part, :node => locale, :news_channel => '2' }
 
-  before { foo_part; bar_part }
+  before { foo_part; bar_part; baz_part }
 
   before { MessageMaker.stub(:make_message) }
 
-  context '#publish' do
-    let(:news) {
-      { 'channels' => [1, 2], 'slug' => 'ololo' }
-    }
+  let(:news) {
+    { 'channels' => [1, 2], 'slug' => 'ololo' }
+  }
 
+  context '#publish' do
     before { MessageMaker.should_receive(:make_message).with('esp.cms.searcher', 'add', "#{foo_page.url}-/ololo") }
-    before { MessageMaker.should_receive(:make_message).with('esp.cms.searcher', 'add', "#{bar_page.url}-/ololo") }
+    before { MessageMaker.should_not_receive(:make_message).with('esp.cms.searcher', 'add', "#{bar_page.url}-/ololo") }
+    before { MessageMaker.should_receive(:make_message).with('esp.cms.searcher', 'add', "#{locale.url}-/ololo") }
 
     specify { subject.publish news }
+  end
+
+  context '#remove' do
+    before { MessageMaker.should_receive(:make_message).with('esp.cms.searcher', 'remove', "#{foo_page.url}-/ololo") }
+    before { MessageMaker.should_not_receive(:make_message).with('esp.cms.searcher', 'remove', "#{bar_page.url}-/ololo") }
+    before { MessageMaker.should_receive(:make_message).with('esp.cms.searcher', 'remove', "#{locale.url}-/ololo") }
+
+    specify { subject.remove news }
   end
 end
