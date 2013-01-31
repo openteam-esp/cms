9e70fb55fb7dcf6bf6ea9b88dc9edf1592687155
Dmitry Lihachev <lda@openteam.ru>
2013-01-31 14:36:58 +0700

    bin/cap db:import работаэ

--- config/deploy/database.rb
+++ config/deploy/database.rb
@@ -35,7 +35,7 @@ namespace :db do
     run_locally("bin/rake sunspot:solr:stop; true")
     run_locally("bin/rake db:drop")
     run_locally("bin/rake db:create")
-    run_locally("ssh #{gateway} -At ssh #{pg_domain} pg_dump -U #{db_username} #{database} | psql #{local_database}")
+    run_locally("ssh #{gateway} -At ssh #{domain} pg_dump -U #{db_username} #{database} -h #{pg_domain}| psql #{local_database}")
     run_locally("bin/rake sunspot:solr:start")
     run_locally("bin/rake db:migrate")
     run_locally("bin/rake db:migrate RAILS_ENV=test")
