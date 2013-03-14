75952f3450fd7b3057bbeef26621aa0b56664146
Dmitry Lihachev <lda@openteam.ru>
2013-03-14 11:31:30 +0700

    change interpretator in FreeBSD

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,7 @@
-log_dir = File.expand_path('../../log', __FILE__)
+log_dir       = File.expand_path('../../log', __FILE__)
+path_to_bash  = (RUBY_PLATFORM =~ /freebsd/) ? '/usr/local/bin/bash' : '/bin/bash'
 
-set :job_template, "/usr/bin/env bash -l -i -c ':job' 1>#{log_dir}/schedule.log 2>#{log_dir}/schedule-errors.log"
+set :job_template, "#{path_to_bash} -l -i -c ':job' 1>>#{log_dir}/schedule.log 2>>#{log_dir}/schedule-errors.log"
 
 every :day do
   rake 'cron'
