04c04a7eb1b564fa4b3e0ae38303921919ea0231
Dmitry Lihachev <lda@openteam.ru>
2013-03-15 15:21:47 +0700

    remove secret_token from git

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Cms::Application.config.secret_token = '7d40ae00150eb199e7661461deb735bb3d9b0f1d5c4ccab817183d1cad5e8dec783f708e10e3336171fcdbedf9fe75e819310729635c239c3766e674dba1370e'
