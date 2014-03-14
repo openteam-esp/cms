5ad0abb6c13f7bf06e80fbe1da0df8a01397d8de
Evgeny Lapin <lev@openteam.ru>
2014-03-14 11:01:40 +0700

    freeze esp-ckeditor version, add esp-ckeditor js to precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,13 @@ Cms::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( text_plugin.js info_plugin.js gallery_pictures_sort.js blue_pages.js )
+  config.assets.precompile += %W[
+                                  text_plugin.js
+                                  info_plugin.js
+                                  gallery_pictures_sort.js
+                                  blue_pages.js
+                                  esp-ckeditor/application.js
+                                ]
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
