608d6703001358128b085930985301c5e21e676f
PeoneEr <peoneerko@yandex.ru>
2015-10-20 10:17:18 +0600

    annotate grouping items model

--- app/models/grouping_item.rb
+++ app/models/grouping_item.rb
@@ -1,3 +1,15 @@
 class GroupingItem < ActiveRecord::Base
   attr_accessible :group, :title
 end
+
+# == Schema Information
+#
+# Table name: grouping_items
+#
+#  id                 :integer          not null, primary key
+#  title              :string(255)
+#  group              :string(255)
+#  navigation_part_id :integer
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
