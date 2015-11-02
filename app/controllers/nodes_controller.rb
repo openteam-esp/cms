2cbd8fb9cbce84d3452d271134a0de443e6b2ffb
Evgeny Lapin <lev@openteam.ru>
2015-11-02 11:13:28 +0600

    first step

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -1,12 +1,21 @@
 class NodesController < ApplicationController
   def show
-    @node = Node.find_by_route(params[:id].force_encoding('iso-8859-1').encode!('utf-8'))
+    params_id = params[:id].force_encoding('iso-8859-1').encode!('utf-8')
+    resource_id = params[:resource_id]
+    @node = Node.find_by_route(params_id)
+
+    unless @node
+      arr_path =  params_id.split('/')
+      resource_id = arr_path.pop
+      params_id = arr_path.join('/')
+      @node = Node.find_by_route(params_id)
+    end
 
     render :file => "#{Rails.root}/public/404", :formats => [:html], :layout => false, :status => 404 and return unless @node
 
     @node.parts_params = params[:parts_params] || {}
     @node.parts_params.merge!(:page => params[:page])
-    @node.resource_id = params[:resource_id]
+    @node.resource_id = resource_id
 
     if params[:region]
       if @part = @node.part_for(params[:region])
