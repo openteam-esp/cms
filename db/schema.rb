8c93a3a3c7f51c7d2b2fff146e36558694486944
maximkuzmin <photographer.max@gmail.com>
2015-12-28 09:06:41 +0600

    minor schema fix

--- db/schema.rb
+++ db/schema.rb
@@ -54,7 +54,6 @@ ActiveRecord::Schema.define(:version => 20151225043810) do
 
   create_table "locale_associations", :force => true do |t|
     t.integer  "site_id"
-    t.string   "title"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
