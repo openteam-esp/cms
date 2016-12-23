969a4da6384ea66b5d7d88685e91636f9e396cc4
Evgeny Lapin <lev@openteam.ru>
2016-12-23 09:19:13 +0700

    fixed error "RSolr::Error::Http - 400 Bad Request Error: start parameter cannot be negative"

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -2,17 +2,18 @@ class NodesController < ApplicationController
   def show
     @node = Node.find_by_route(params[:id].force_encoding('iso-8859-1').encode!('utf-8'))
 
-    render :file => "#{Rails.root}/public/404", :formats => [:html], :layout => false, :status => 404 and return unless @node
+    render :file => %[#{Rails.root}/public/404], formats: [:html], layout: false, status: 404 and return unless @node
 
     @node.parts_params = params[:parts_params] || {}
-    @node.parts_params.merge!(:page => params[:page])
+    page = params[:page].to_i <= 0 ? 1 : params[:page].to_i
+    @node.parts_params.merge!(page: page)
     @node.resource_id = params[:resource_id]
 
     if params[:region]
       if @part = @node.part_for(params[:region])
-        render :json => @part.to_json
+        render json: @part.to_json
       else
-        render :json => 'not found', :status => 404
+        render json: 'not found', status: 404
       end
 
       return
@@ -20,7 +21,7 @@ class NodesController < ApplicationController
 
     respond_to do |format|
       format.html
-      format.json { render :json => @node.to_json }
+      format.json { render json: @node.to_json }
     end
   end
 end
